72badb081a5b6e3ce9c4155e25524d5118921746
Harrison-X-Black <115744146+Harrison-X-Black@users.noreply.github.com>
2022-10-18 13:24:35 -0400

    modules 1

---
+++ Modules/lib.js
@@ -0,0 +1,7 @@
+export function halfOf(x) {
+    return x / 2
+}
+
+export function multiplay(x, y) {
+    return x / 2
+}
---
+++ Modules/main.js
@@ -0,0 +1,3 @@
+import { halfOf, multiply } from "./lib";
+console.log("halfOf: " + halfOf(84) );
+console.log("multiply: " + multiply(84, 2));
